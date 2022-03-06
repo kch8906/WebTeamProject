24e848821d93c0edae0a9e79d3d736ca0745a3b8
Jiyeon Lee <ljy9969@gmail.com>
2022-03-06 18:33:23 +0900

    trying to fix PR error on github_attempt 2

--- corona_19/urls.py
+++ corona_19/urls.py
@@ -6,18 +6,18 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),                                          # Admin Page
     path('', TemplateView.as_view(template_name='index.html'), name='home'),  # Home Page
+    path('admin/', admin.site.urls),                                          # Admin Page
     path('main/', include('main.urls')),                                      # login 후 Home Page
     path('users/', include('users.urls')),                                    # login, 회원가입 page
     path('maps/', include('maps.urls')),                                      # Maps Page
+    path('bbs/', include('probbs.urls')),                                     # Pro BBS Page(전문의에게 물어보세요 게시판)
     path('guide/', include('guide.urls')),                                    # Guide Page(코로나 증상 및 예방 조치)
     path('diagnosis/', include('diagnosis.urls')),                            # Diagnosis Page(자가진단)
-    # path('probbs/', include('probbs.urls')),                                        # Pro BBS Page(전문의에게 물어보세요 게시판)
 ] + static(settings.MEDIA_URL,
            document_root=settings.MEDIA_ROOT)
-           
+
 if settings.DEBUG:
     if "debug_toolbar" in settings.INSTALLED_APPS:
         import debug_toolbar
-        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + 
+        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
