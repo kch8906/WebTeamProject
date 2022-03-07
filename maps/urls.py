5a19af11623a9facfcc1ef0a83702c903d9f2535
unknown <wonjiprog@gmail.com>
2022-03-07 09:17:09 +0900

    pull origin wonji branch

---
+++ maps/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from maps import views
+
+app_name = 'map'
+urlpatterns = [
+    path('positions/', views.pos_hospital, name='pos_hospital'),
+    path('test/', views.test, name='test')
+]
