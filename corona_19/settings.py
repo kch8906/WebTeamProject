801ad5f40f59f4e60631129997fb1e30a4f8d371
unknown <wonjiprog@gmail.com>
2022-03-08 09:48:06 +0900

    settings file- password change

--- corona_19/settings.py
+++ corona_19/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'corona_db',
         'USER': 'root',
-        'PASSWORD': 'ckdgus8906!',
+        'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
